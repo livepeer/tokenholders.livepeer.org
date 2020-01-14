37c1922682d79954ce608afb812b4b796e1e0f26
Adam Soffer <adam@livepeer.org>
2020-01-14 12:17:45 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,15 @@ export default class MyApp extends App {
             href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,500&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-111259858-1"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-111259858-1');`
+            }}
+          />
           <style
             dangerouslySetInnerHTML={{
               __html: `@font-face {
